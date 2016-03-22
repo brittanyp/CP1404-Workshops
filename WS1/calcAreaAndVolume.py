859b931d32b1e63e5e2ccc49a1ab1783c22da450
brittanyp <brittany.peever@my.jcu.edu.au>
2016-03-22 18:22:00 +1000

    W4: do it yourself tasks

--- WS1/calcAreaAndVolume.py
+++ WS1/calcAreaAndVolume.py
@@ -1,7 +1,25 @@
 print('Area Calculator')
-width = int(input('enter width: '))
-height = int(input('enter height'))
-depth = int(input('enter depth'))
+
+def validate_number(str):
+    valid = False
+    num = input(str)
+    while valid == False:
+
+        if num.isdecimal():
+            if int(num) > 0:
+                valid=True
+            else:
+                print("Enter number in range")
+                num=input(str)
+        else:
+            print("Enter valid number")
+            num=input(str)
+
+    return(num);
+
+width = int(validate_number('enter width: '))
+height = int(validate_number('enter height: '))
+depth = int(validate_number('enter depth: '))
 
 area = width * height * depth
 
